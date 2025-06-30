9ae00a623d28ded0504062033644530a2447242d
Jxzton <danieljxzt@gmail.com>
2025-06-30 18:21:27 +0200

    backend update

--- pet-shop-backend/index.js
+++ pet-shop-backend/index.js
@@ -1,24 +1,28 @@
-const { request } = require('express');
 const express = require('express');
+const cors = require('cors');
+const sequelize = require('./database/database');
+
 const categories = require('./routes/categories');
 const sale = require('./routes/sale');
 const order = require('./routes/order');
 const products = require('./routes/products');
-const sequelize = require('./database/database');
-const cors = require('cors')
+
 const Category = require('./database/models/category');
 const Product = require('./database/models/product');
-const PORT = 3333;
+
+const PORT = process.env.PORT || 3333;
 
 Category.hasMany(Product);
 
 const app = express();
-app.use(express.static('public'))
-app.use(cors({
-    origin: '*'
-}));
 
-app.use(express.urlencoded());
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Routes
 app.use('/categories', categories);
 app.use('/products', products);
 app.use('/sale', sale);
@@ -28,22 +32,15 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.use(express.json());
+const start = async () => {
+  try {
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-const start = async () =>{
-    try{
-        await sequelize.sync().then(
-            result => {/*console.log(result) */},
-            err => console.log(err)
-        );
-        
-        app.listen(PORT, ()=>{
-            console.log(`\n\nServer started on ${PORT} port...`)
-        })
-    }catch(err){
-        console.log(err);
-    }
-}
 start();
-
-// app.listen('3333');
