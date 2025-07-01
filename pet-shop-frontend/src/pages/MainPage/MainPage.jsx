d835ddc90d465fb8203865e4c466d61d8292cc4d
Jxzton <danieljxzt@gmail.com>
2025-07-01 15:32:12 +0200

    responsive-mainpage

--- pet-shop-frontend/src/pages/MainPage/MainPage.jsx
+++ pet-shop-frontend/src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import styles from "./MainPage.module.css";
-import { Link, useLocation } from "react-router-dom";
-import CarouselCategories from "../../components/CarouselCategories/CarouselCategories";
-import CarouselSales from "../../components/CarouselSales/CarouselSales";
-import DiscountForm from "../../components/DiscountForm/DiscountForm";
+import React from 'react';
+import styles from './MainPage.module.css';
+import { Link, useLocation } from 'react-router-dom';
+import CarouselCategories from '../../components/CarouselCategories/CarouselCategories';
+import CarouselSales from '../../components/CarouselSales/CarouselSales';
+import DiscountForm from '../../components/DiscountForm/DiscountForm';
 
 export default function MainPage() {
   const location = useLocation();
@@ -15,26 +15,18 @@ export default function MainPage() {
     <div className={styles.container}>
       <div className={styles.banner}>
         <h1>Amazing Discounts on Pets Products!</h1>
-        <Link style={{ alignSelf: "start" }} to="/sales">
-          <div className={styles.banner_bnt} style={{ padding: "16px 56px" }}>
+        <Link style={{ alignSelf: 'start' }} to="/sales">
+          <div className={styles.banner_bnt} style={{ padding: '16px 56px' }}>
             Check out
           </div>
         </Link>
       </div>
       <div className={styles.categories_container}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            marginBottom: "32px",
-          }}
-        >
+        <div className={styles.categories_div}>
           <h2 className={styles.categories_main_title}>Categories</h2>
-          <div style={{ flexGrow: 1, height: "1px", backgroundColor: "#DDDDDD" }}></div>
+          <div className={styles.categories_line}></div>
           <div className={styles.categories_bnt_container}>
-            <Link style={{ alignSelf: "end" }} to="/categories">
+            <Link style={{ alignSelf: 'end' }} to="/categories">
               <div className={styles.categories_bnt}>All categories</div>
             </Link>
           </div>
@@ -45,19 +37,11 @@ export default function MainPage() {
         <DiscountForm />
       </div>
       <div className={styles.sales_container}>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-            marginBottom: "32px",
-          }}
-        >
+        <div className={styles.sales_div} >
           <h2 className={styles.sales_main_title}>Sale</h2>
-          <div style={{ flexGrow: 1, height: "1px", backgroundColor: "#DDDDDD" }}></div>
+          <div className={styles.categories_line}></div>
           <div className={styles.sales_bnt_container}>
-            <Link style={{ alignSelf: "end" }} to="/sales">
+            <Link style={{ alignSelf: 'end' }} to="/sales">
               <div className={styles.sales_bnt}>All sales</div>
             </Link>
           </div>
@@ -67,4 +51,3 @@ export default function MainPage() {
     </div>
   );
 }
-
